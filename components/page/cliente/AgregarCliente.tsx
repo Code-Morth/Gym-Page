0d45703c384b86e3b55eaa20ff4f2186f8987c26
CodeMorth <kevinsalinasvd@gmail.com>
2024-05-28 23:29:20 -0500

    todos los clientes

--- components/page/cliente/AgregarCliente.tsx
+++ components/page/cliente/AgregarCliente.tsx
@@ -1,46 +1,65 @@
-'use client'
-import React from "react"
+"use client"
+import React, { useRef } from "react"
+import apisPeticion from "@/api/apisPeticion"
+import getConfig from "../../../utils/getConfig"
+import { useAlerts } from "../../../hook/useAlerts"
+import { Toast } from "primereact/toast"
+import axios from "axios"
 
 const AgregarCliente = () => {
+  const { url } = apisPeticion()
+  const dataRef = useRef<any>(null)
+  const { show, toast } = useAlerts()
 
-    const handleLogin = (event: any) => {
+  const handleLogin = (event: any) => {
+    event.preventDefault()
 
-        event.preventDefault()
-    
-        const dataForm = Object.fromEntries(new FormData(event.target))
+    const dataForm = Object.fromEntries(new FormData(event.target))
 
-        console.log(dataForm)
-    
-      }
+    axios
+      .post(`${url}/client`, dataForm, getConfig())
+      .then((res) => {
+        if (res.data.success) {
+          show("Usuario Creado Exitosamente")
+          dataRef.current.reset()
+        }
+      })
+      .catch((err) => console.log(err))
+
+    console.log(dataForm)
+  }
 
   return (
     <div className="AgregarCliente">
       <div className="agregar-cliente-container">
         <h1>Registro de cliente</h1>
-        <form onSubmit={handleLogin} className="form-container">
+        <form ref={dataRef} onSubmit={handleLogin} className="form-container">
           <div className="data-container">
             <div className="form-left">
-              <label htmlFor="userName">Nombre de usuario</label>
-              <input required name="userName" type="text" />
-              <label htmlFor="secondSurname">Segundo apellido</label>
-              <input name="secondSurname" type="text" />
-              <label htmlFor="telephone">Telefono</label>
-              <input required name="telephone" type="number" />
-              <label htmlFor="membership">Membresia</label>
-              <input name="membership" type="text" />
+              <label htmlFor="first_name">Nombre de usuario</label>
+              <input required name="first_name" type="text" />
+              <label htmlFor="last_name2">Segundo apellido</label>
+              <input name="last_name2" type="text" />
+              <label htmlFor="phone">Telefono</label>
+              <input required name="phone" type="number" />
+              <label htmlFor="fk_membership">Membresia</label>
+              <input name="fk_membership" type="number" />
             </div>
             <div className="form-rigth">
-              <label htmlFor="firstSurname">Primer apellido</label>
-              <input name="firstSurname" type="text" />
+              <label htmlFor="last_name1">Primer apellido</label>
+              <input name="last_name1" type="text" />
               <label htmlFor="email">Correo</label>
               <input required name="email" type="email" />
               <label htmlFor="address">Direccion</label>
               <input name="address" type="text" />
+              <label htmlFor="ci">Ci</label>
+              <input name="ci" type="number" />
             </div>
           </div>
           <button className="button-default">Agregar cliente</button>
         </form>
       </div>
+      <Toast ref={toast} position="top-center" />
     </div>
   )
 }
--- components/page/cliente/TodosLosClientes.tsx
+++ components/page/cliente/TodosLosClientes.tsx
@@ -3,24 +3,66 @@ import { DataTable } from "primereact/datatable"
 import { Column } from "primereact/column"
 import todosLosUsuarios from "../../../json/todosLosUsuarios.json"
 import { useEffect, useState } from "react"
+import apisPeticion from "@/api/apisPeticion"
+import getConfig from "../../../utils/getConfig"
+import axios from "axios"
 
 const TodosLosClientes = () => {
   const [customers, setCustomers] = useState<any>([])
+  const [memberShip, setmemberShip] = useState<any>()
+  const { url } = apisPeticion()
 
   useEffect(() => {
     setCustomers(todosLosUsuarios)
   }, [])
 
-  return (  
+  useEffect(() => {
+    axios
+      .get(`${url}/client`, getConfig())
+      .then((res) => setCustomers(res.data.data))
+      .catch((err) => console.log(err))
+
+    axios
+      .get(`${url}/membership`, getConfig())
+      .then((res) => setmemberShip(res.data.data))
+      .catch((err) => console.log(err))
+  }, [])
+  const filterCustomerExpired = customers.filter((user: any) => {
+    const statusUser = user?.status !== "active"
+    return statusUser
+  })
+
+  const filterCustomerActive = customers.filter((user: any) => {
+    const statusUser = user?.status === "active"
+    return statusUser
+  })
+
+  const accionUser = (rowData: any) => {
+    return <button onClick={()=>console.log(rowData.id)}>Editar</button>
+  }
+
+  const columnMemberShip = (rowData: any) => {
+    const filterMemberShip = memberShip?.filter((data: any) => {
+      const member = data.id === rowData.fk_membership
+
+      return member
+    })
+
+    return <span>{filterMemberShip[0].name}</span>
+  }
+
+  console.log("memberShip", memberShip)
+
+  return (
     <div className="TodosLosClientes">
       <div className="todos-los-clientes-container">
-        <input type="text" placeholder='Buscar cliente' />
+        <input type="text" placeholder="Buscar cliente" />
         <div className="table-1">
           <h1>Clientes activos</h1>
           <div className="tabla-container">
             <DataTable
               className="data-table"
-              value={customers}
+              value={filterCustomerActive}
               paginator
               rows={5}
               rowsPerPageOptions={[5, 10, 25, 50]}
@@ -28,19 +70,19 @@ const TodosLosClientes = () => {
             >
               <Column
                 className="column"
-                field="fullName"
+                field="first_name"
                 header="Nombre completo"
                 style={{ width: "8%" }}
               ></Column>
               <Column
                 className="column"
-                field="firstSurname"
+                field="last_name1"
                 header="Primer apellido"
                 style={{ width: "8%" }}
               ></Column>
               <Column
                 className="column"
-                field="secondSurname"
+                field="last_name2"
                 header="Segundo apellido"
                 style={{ width: "8%" }}
               ></Column>
@@ -52,32 +94,15 @@ const TodosLosClientes = () => {
               ></Column>
               <Column
                 className="column"
-                field="startDate"
-                header="Fecha de inicio"
-                style={{ width: "8%" }}
-              ></Column>
-              <Column
-                className="column"
-                field="finalDate"
-                header="Fecha final"
-                style={{ width: "8%" }}
-              ></Column>
-              <Column
-                className="column"
-                field="entryTime"
-                header="Horario de entrada"
+                field="fk_membership"
+                header="Membresia"
                 style={{ width: "10%" }}
+                body={columnMemberShip}
               ></Column>
               <Column
                 className="column"
-                field="closingHour"
-                header="Horario de salida"
-                style={{ width: "10%" }}
-              ></Column>
-              <Column
-                className="column"
-                field="ci"
-                header="Numero de carnet"
+                field="phone"
+                header="Telefono"
                 style={{ width: "10%" }}
               ></Column>
               <Column
@@ -88,21 +113,22 @@ const TodosLosClientes = () => {
               ></Column>
               <Column
                 className="column"
-                field="password"
-                header="Contraseña"
+                field="createdAt"
+                header="Fecha de registro"
                 style={{ width: "10%" }}
               ></Column>
               <Column
                 className="column"
-                field="userType"
-                header="Rol"
+                field="status"
+                header="Estado de membresia"
                 style={{ width: "10%" }}
               ></Column>
               <Column
                 className="column"
-                field="actions"
+                field="id"
                 header="Acciones"
                 style={{ width: "10%" }}
+                body={accionUser}
               ></Column>
             </DataTable>
           </div>
@@ -112,27 +138,27 @@ const TodosLosClientes = () => {
           <div className="tabla-container">
             <DataTable
               className="data-table"
-              value={customers}
+              value={filterCustomerExpired}
               paginator
               rows={5}
               rowsPerPageOptions={[5, 10, 25, 50]}
               tableStyle={{ minWidth: "50rem" }}
             >
-              <Column
+               <Column
                 className="column"
-                field="fullName"
+                field="first_name"
                 header="Nombre completo"
                 style={{ width: "8%" }}
               ></Column>
               <Column
                 className="column"
-                field="firstSurname"
+                field="last_name1"
                 header="Primer apellido"
                 style={{ width: "8%" }}
               ></Column>
               <Column
                 className="column"
-                field="secondSurname"
+                field="last_name2"
                 header="Segundo apellido"
                 style={{ width: "8%" }}
               ></Column>
@@ -144,32 +170,15 @@ const TodosLosClientes = () => {
               ></Column>
               <Column
                 className="column"
-                field="startDate"
-                header="Fecha de inicio"
-                style={{ width: "8%" }}
-              ></Column>
-              <Column
-                className="column"
-                field="finalDate"
-                header="Fecha final"
-                style={{ width: "8%" }}
-              ></Column>
-              <Column
-                className="column"
-                field="entryTime"
-                header="Horario de entrada"
+                field="fk_membership"
+                header="Membresia"
                 style={{ width: "10%" }}
+                body={columnMemberShip}
               ></Column>
               <Column
                 className="column"
-                field="closingHour"
-                header="Horario de salida"
-                style={{ width: "10%" }}
-              ></Column>
-              <Column
-                className="column"
-                field="ci"
-                header="Numero de carnet"
+                field="phone"
+                header="Telefono"
                 style={{ width: "10%" }}
               ></Column>
               <Column
@@ -180,21 +189,22 @@ const TodosLosClientes = () => {
               ></Column>
               <Column
                 className="column"
-                field="password"
-                header="Contraseña"
+                field="createdAt"
+                header="Fecha de registro"
                 style={{ width: "10%" }}
               ></Column>
               <Column
                 className="column"
-                field="userType"
-                header="Rol"
+                field="status"
+                header="Estado de membresia"
                 style={{ width: "10%" }}
               ></Column>
               <Column
                 className="column"
-                field="actions"
+                field="id"
                 header="Acciones"
                 style={{ width: "10%" }}
+                body={accionUser}
               ></Column>
             </DataTable>
           </div>
