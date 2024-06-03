fa6f45fb54f2435ffd693102b6d1ca29a125cdb6
CodeMorth <kevinsalinasvd@gmail.com>
2024-06-03 18:58:13 -0500

    permissions

--- components/page/ventas/Estadisticas.tsx
+++ components/page/ventas/Estadisticas.tsx
@@ -4,6 +4,7 @@ import axios from "axios"
 import { Chart } from "primereact/chart"
 import React, { useEffect, useState } from "react"
 import getConfig from "../../../utils/getConfig"
+import dayjs from "dayjs"
 
 // Función para generar colores aleatorios
 const generateRandomColor = (opacity: any) => {
@@ -23,21 +24,29 @@ const generateColorPalette = (numColors: any) => {
 }
 
 const Estadisticas = () => {
+  const actualDate = dayjs().format("DD-MM-YYYY")
+  const datePlusOneMonth = dayjs().add(1, "month").format("DD-MM-YYYY")
   const [chartData, setChartData] = useState({})
-  const [dateStart, setdateStart] = useState<any>()
-  const [dateEnd, setdateEnd] = useState<any>()
+  const [dateStart, setdateStart] = useState<any>(actualDate)
+  const [dateEnd, setdateEnd] = useState<any>(datePlusOneMonth)
   const { url } = apisPeticion()
 
   useEffect(() => {
+    const dateStartFormat = dayjs(dateStart).format("DD-MM-YYYY")
+    const dateEndFormat = dayjs(dateEnd).format("DD-MM-YYYY")
+
     axios
-      .get(`${url}/order?page=0&size=999999999999999`, getConfig())
+      .get(
+        `${url}/order?page=0&size=999999999999999&order_date_ini=${dateStartFormat}&order_date_end=${dateEndFormat}`,
+        getConfig()
+      )
       .then((res) => {
         const newData1 = res.data.data.map((e: any) => ({
           date: e.order_date,
           detail: e.detail,
         }))
 
-        const meses:any = {
+        const meses: any = {
           "01": "Enero",
           "02": "Febrero",
           "03": "Marzo",
@@ -49,21 +58,24 @@ const Estadisticas = () => {
           "09": "Septiembre",
           "10": "Octubre",
           "11": "Noviembre",
-          "12": "Diciembre"
-        };
-        
-        const groupedData = newData1.reduce((acc:any, current:any) => {
-          const dateParts = current.date.split("-"); // Dividir la fecha en partes (año, mes, día)
-          const month = dateParts[0] + "-" + dateParts[1]; // Obtener el año y el mes
+          "12": "Diciembre",
+        }
+
+        const groupedData = newData1.reduce((acc: any, current: any) => {
+          const dateParts = current.date.split("-") // Dividir la fecha en partes (año, mes, día)
+          const month = dateParts[0] + "-" + dateParts[1] // Obtener el año y el mes
           if (!acc[month]) {
-            acc[month] = { date: meses[dateParts[1]] + " " + dateParts[0], detail: [] };
+            acc[month] = {
+              date: meses[dateParts[1]] + " " + dateParts[0],
+              detail: [],
+            }
           }
-          acc[month].detail = acc[month].detail.concat(current.detail);
-          return acc;
-        }, {});
-        
-        const result = Object.values(groupedData);
-        
+          acc[month].detail = acc[month].detail.concat(current.detail)
+          return acc
+        }, {})
+
+        const result = Object.values(groupedData)
+
         const labelsArray = result.map((e: any) => e.date)
 
         const sum = (x: any, y: any) => {
@@ -103,14 +115,11 @@ const Estadisticas = () => {
         }
 
         setChartData(data)
-
       })
       .catch((err) => console.log(err))
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-
-  console.log("chartData", chartData)
+  }, [dateStart, dateEnd])
 
   return (
     <div className="Estadisticas main-page">
